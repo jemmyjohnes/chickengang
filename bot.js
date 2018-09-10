9c61ff86057e540a64405c00268b204f6de8cbff
jemmyjohnes <43142384+jemmyjohnes@users.noreply.github.com>
2018-09-11 01:08:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,4 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
-
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
-
-if(!process.send) {
-
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
-});//---------------------------------------------------------------------
+//---------------------------------------------------------------------
 // Bot
 // Contains functions for controlling the bot.
 //---------------------------------------------------------------------
