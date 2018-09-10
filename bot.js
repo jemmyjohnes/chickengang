f4a115eeb94141d035192b50a1febeb0785ec10a
jemmyjohnes <43142384+jemmyjohnes@users.noreply.github.com>
2018-09-11 01:07:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ Files.initStandalone();
 process.on('message', function(content) {
 	Files.initBotTest(content);
 });
-
-}
 client.login(process.env.NDcxNzU0MzkxOTE4MjE1MTY4.Dng1cA.dWVM-fFHmZnEkjJEqHCSX_vZZ5Y)
+}
+
 
 
